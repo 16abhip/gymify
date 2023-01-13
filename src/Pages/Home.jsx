40188363860a392da0876ff9898dc42ab0f4a7b0
16_abhip <abhip16singh@gmail.com>
2023-01-13 15:08:25 +0530

    initial commit and setup done

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import { Box } from '@mui/system'
+import SearchExercises from '../components/SearchExercises'
+import Exercises from '../components/Exercises'
+import HeroBanner from '../components/HeroBanner'
+const Home = () => {
+  return (
+    <Box>
+      <HeroBanner/>
+      <SearchExercises/>
+      <Exercises/>
+    </Box>
+  )
+}
+
+export default Home
