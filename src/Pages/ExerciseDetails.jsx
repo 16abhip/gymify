3c20671ff9398d54441921c31216f15a8e7af748
16_abhip <abhip16singh@gmail.com>
2023-01-14 13:29:37 +0530

    Fetch exercise videos from rapidapi youtube video api

--- src/Pages/ExerciseDetails.jsx
+++ src/Pages/ExerciseDetails.jsx
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 import { Box } from '@mui/system'
-import {exerciseOptions, fetchData} from '../utils/fetchData'
+import {exerciseOptions, fetchData, youtubeOptions} from '../utils/fetchData'
 import Detail from '../components/Detail'
 import ExerciseVideos from '../components/ExerciseVideos'
 import SimilarExercises from '../components/SimilarExercises'
 const ExerciseDetails = () => { 
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
   const {id} = useParams();
   useEffect(() => {
     const fetchExercisesData = async() =>{
@@ -14,13 +15,16 @@ const ExerciseDetails = () => {
       const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
       const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
       setExerciseDetail(exerciseDetailData);
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`, youtubeOptions);
+      setExerciseVideos(exerciseVideosData.contents);
     }
+    
     fetchExercisesData();
   }, [id])
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos/>
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name}/>
       <SimilarExercises/>
     </Box>
   )
